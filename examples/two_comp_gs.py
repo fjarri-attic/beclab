e1410d176b1e074762fc44a6b4d93e1e82e79dc8
Bogdan Opanchuk <mantihor@gmail.com>
2010-08-25 23:06:14 +1000

    examples/two_comp_gs: some minor fixes for convenience of use

--- examples/two_comp_gs.py
+++ examples/two_comp_gs.py
@@ -12,28 +12,34 @@ def testTwoCompGS(gpu):
 	gs = GPEGroundState(env, constants)
 	sp = SliceCollector(env, constants, pulse=None)
 
-	cloud = gs.createCloud(two_component=True, precision=1e-9)
+	# in fact, accurate two-component ground state calculations requires
+	# precision of at least 1e-8, but this is just a test
+	cloud = gs.createCloud(two_component=True, precision=1e-7)
 	sp(0, cloud)
 
 	# render
 	times, a_xy, a_yz, b_xy, b_yz = sp.getData()
 
-	a_data = HeightmapData("1 component", a_yz[0].transpose(), xmin=-constants.zmax, xmax=constants.zmax,
+	a_data = HeightmapData("1st component", a_yz[0].transpose(),
+		xmin=-constants.zmax * 1e6, xmax=constants.zmax * 1e6,
 		xname="Z, $\\mu$m", yname="Y, $\\mu$m",
-		ymin=-constants.ymax, ymax=constants.ymax, zmin=0)
+		ymin=-constants.ymax * 1e6, ymax=constants.ymax * 1e6, zmin=0)
 	a_plot = HeightmapPlot(a_data)
 
-	b_data = HeightmapData("2 component", b_yz[0].transpose(), xmin=-constants.zmax, xmax=constants.zmax,
+	b_data = HeightmapData("2nd component", b_yz[0].transpose(),
+		xmin=-constants.zmax * 1e6, xmax=constants.zmax * 1e6,
 		xname="Z, $\\mu$m", yname="Y, $\\mu$m",
-		ymin=-constants.ymax, ymax=constants.ymax, zmin=0)
+		ymin=-constants.ymax * 1e6, ymax=constants.ymax * 1e6, zmin=0)
 	b_plot = HeightmapPlot(b_data)
 
 	return a_plot, b_plot
 
+print "CPU variant"
 p1, p2 = testTwoCompGS(False)
 p1.save("two_comp_gs_cpu_a.pdf")
 p2.save("two_comp_gs_cpu_b.pdf")
 
+print "GPU variant"
 p1, p2 = testTwoCompGS(True)
 p1.save("two_comp_gs_gpu_a.pdf")
 p2.save("two_comp_gs_gpu_b.pdf")
