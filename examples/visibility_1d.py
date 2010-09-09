171dce62ade778e24fbe9cd73b798f052e55683a
Bogdan Opanchuk <mantihor@gmail.com>
2010-09-09 16:39:07 +1000

    examples/visibility_1d: using only visibility collector
    Others use cloud.copy() which takes a lot of time for GPU (due to
    kernel compilation). I will fix it later, and now I need
    as much speed as I can get.

--- examples/visibility_1d.py
+++ examples/visibility_1d.py
@@ -22,13 +22,13 @@ def test(gpu=False, ensembles=256, nvz=32, dt_evo=1e-5, a12=97.9,
 		double_precision=(not gpu))
 	env = Environment(gpu=gpu)
 
-	evolution = SplitStepEvolution2(env, constants)
+	evolution = SplitStepEvolution(env, constants)
 
 	gs = GPEGroundState(env, constants)
 	pulse = Pulse(env, constants)
 	v = VisibilityCollector(env, constants, verbose=False)
-	p = ParticleNumberCollector(env, constants, pulse=pulse, matrix_pulse=True, verbose=False)
-	a = AxialProjectionCollector(env, constants, matrix_pulse=True, pulse=pulse)
+	#p = ParticleNumberCollector(env, constants, pulse=pulse, matrix_pulse=True, verbose=False)
+	#a = AxialProjectionCollector(env, constants, matrix_pulse=True, pulse=pulse)
 
 	#ps = ParticleStatistics(env, constants)
 
@@ -41,7 +41,7 @@ def test(gpu=False, ensembles=256, nvz=32, dt_evo=1e-5, a12=97.9,
 	pulse.apply(cloud, math.pi * 0.5, matrix=True)
 
 	t1 = time.time()
-	evolution.run(cloud, 0.05, callbacks=[v, p, a], callback_dt=0.0005, noise=noise)
+	evolution.run(cloud, 0.05, callbacks=[v], callback_dt=0.0005, noise=noise)
 	env.synchronize()
 	t2 = time.time()
 	print "Time spent: " + str(t2 - t1) + " s"
@@ -61,24 +61,21 @@ def test(gpu=False, ensembles=256, nvz=32, dt_evo=1e-5, a12=97.9,
 	vis_data = XYData(name, times, vis, ymin=0, ymax=1,
 		xname="Time, s", yname="Visibility")
 
-	times, N1, N2, N = p.getData()
-	pop_data = XYData(name, times, N1,
-		ymin=0, ymax=60, xname="Time, s", yname="Population, N1")
+	#times, N1, N2, N = p.getData()
+	#pop_data = XYData(name, times, N1,
+	#	ymin=0, ymax=60, xname="Time, s", yname="Population, N1")
 
-	times, picture = a.getData()
-	axial_data = HeightmapData(name, picture,
-		xmin=0, xmax=50,
-		ymin=-constants.zmax * 1e6,
-		ymax=constants.zmax * 1e6,
-		zmin=0, zmax=1,
-		xname="Time, ms", yname="z, $\\mu$m", zname="Spin projection")
+	#times, picture = a.getData()
+	#axial_data = HeightmapData(name, picture,
+	#	xmin=0, xmax=50,
+	#	ymin=-constants.zmax * 1e6,
+	#	ymax=constants.zmax * 1e6,
+	#	zmin=0, zmax=1,
+	#	xname="Time, ms", yname="z, $\\mu$m", zname="Spin projection")
 
-	return vis_data, pop_data, axial_data
+	return vis_data
 
-v1, _, _ = test(noise=False)
-v2, _, _ = test(noise=False, losses=False)
-v3, _, _ = test(noise=False, losses=False, equilibration_time=0.03)
-v4, _, _ = test(noise=False, losses=True)
-v5, _, _ = test(noise=False, losses=True, equilibration_time=0.03)
+#v1 = test(noise=False, gpu=True)
+#v2 = test(noise=False, gpu=False)
 
-XYPlot([v1, v2, v3, v4, v5], location="upper right").save("1d_visibility.pdf")
+XYPlot([test(noise=False, gpu=True, a12=80.8) for i in xrange(10)], location="upper right").save("1d_visibility.pdf")
