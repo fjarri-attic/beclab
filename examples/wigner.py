2c52d1a0a6e0de8694df65f1750b540d78e38c0c
Bogdan Opanchuk <mantihor@gmail.com>
2010-09-29 23:03:08 +1000

    examples/wigner.py: using it for phase noise collection

--- examples/wigner.py
+++ examples/wigner.py
@@ -6,13 +6,15 @@ from beclab import *
 from beclab.state import ParticleStatistics
 
 def testWigner(gpu):
-	constants = Constants(Model(N=30000, detuning=-41, nvx=16, nvy=16, nvz=128,
-		ensembles=2), double_precision=False)
+	constants = Constants(Model(N=40000, detuning=-41, nvx=16, nvy=16, nvz=128,
+		ensembles=4, e_cut=1e6), double_precision=(False if gpu else True))
 	env = Environment(gpu=gpu)
-	evolution = SplitStepEvolution2(env, constants)
+	evolution = SplitStepEvolution(env, constants)
 	pulse = Pulse(env, constants)
 	a = VisibilityCollector(env, constants, verbose=True)
 	b = ParticleNumberCollector(env, constants, verbose=True, pulse=pulse, matrix_pulse=True)
+	p = ParticleStatistics(env, constants)
+	n = PhaseNoiseCollector(env, constants)
 
 	gs = GPEGroundState(env, constants)
 
@@ -23,7 +25,7 @@ def testWigner(gpu):
 	pulse.apply(cloud, 0.5 * math.pi, matrix=True)
 
 	t1 = time.time()
-	evolution.run(cloud, 0.4, callbacks=[a, b], callback_dt=0.01)
+	evolution.run(cloud, 0.05, callbacks=[n], callback_dt=0.01, noise=False)
 	env.synchronize()
 	t2 = time.time()
 	print "Time spent: " + str(t2 - t1) + " s"
@@ -31,11 +33,15 @@ def testWigner(gpu):
 	#times, Na, Nb, N = b.getData()
 	#XYPlot([XYData("test", times, (Na-Nb)/N, ymin=-1, ymax=1, xname="Time, s")]).save('test.pdf')
 
-	times, vis = a.getData()
-	vis = XYData("noise", times, vis, ymin=0, ymax=1,
-		xname="Time, ms", yname="Visibility")
-	vis = XYPlot([vis])
-	vis.save('test.pdf')
+	#times, vis = a.getData()
+	#vis = XYData("noise", times, vis, ymin=0, ymax=1,
+	#	xname="Time, ms", yname="Visibility")
+	#vis = XYPlot([vis])
+	#vis.save('test.pdf')
 
-testWigner(False)
+	times, noise = n.getData()
+	XYPlot([XYData("test", times * 1000, noise, ymin=0, xname="Time, ms")]).save(
+		'phase_noise_' + str(env) + '.pdf')
+
+#testWigner(False)
 testWigner(True)
