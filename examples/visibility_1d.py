44942c9ddf1e86f63876c2408812a2d765512632
Bogdan Opanchuk <mantihor@gmail.com>
2010-09-29 23:02:42 +1000

    examples/visibility_1d: added some showcases

--- examples/visibility_1d.py
+++ examples/visibility_1d.py
@@ -77,9 +77,67 @@ def test(gpu=False, ensembles=256, nvz=32, dt_evo=1e-5, a12=97.9,
 	#	zmin=0, zmax=1,
 	#	xname="Time, ms", yname="z, $\\mu$m", zname="Spin projection")
 
-	return vis_data
+	return vis_data, pop_data
 
-#v1 = test(noise=False, gpu=True)
-#v2 = test(noise=False, gpu=False)
+XYPlot([test(noise=False, gpu=False, nvz=32, losses=False, wigner=False, a12=(100.4 + 98.13)/2)[0]]).save("for_m.pdf")
+exit(1)
+#XYPlot([
+#	test(noise=True, gpu=False, a12=97.9, nvz=16, ensembles=64),
+#	test(noise=True, gpu=True, a12=97.9, nvz=16, ensembles=64)
+#	], location="upper right").save("1d_visibility.pdf")
+#XYPlot([test(noise=False, gpu=True, a12=97.9, nvz=1024, ensembles=64)[0] for i in xrange(1)],
+#	location="upper right").save("1d_visibility.pdf")
+#XYPlot([test(noise=True, gpu=True, a12=97.9, nvz=32, ensembles=512)[0] for i in xrange(5)],
+#	).save("test1.pdf")
+#XYPlot([test(noise=True, gpu=True, a12=97.9, nvz=32, ensembles=512)[0] for i in xrange(5)],
+#	legend=False).save("test2.pdf")
+#XYPlot([test(noise=True, gpu=True, a12=97.9, nvz=32, ensembles=512)[0] for i in xrange(5)],
+#	legend=False, gradient=True).save("test3.pdf")
 
-XYPlot([test(noise=False, gpu=True, a12=80.8) for i in xrange(10)], location="upper right").save("1d_visibility.pdf")
+#XYPlot([test(noise=False, gpu=True, a12=80.8, nvz=x)[0] for x in [16,32,64,128,256,512,1024,2048]],
+#	location="lower left").save("1d_visibility.pdf")
+#XYPlot([
+#	test(noise=True, gpu=False, a12=97.9, nvz=16, ensembles=64)
+#], location="upper right").save("1d_visibility.pdf")
+
+#v1, p1 = test(noise=True, gpu=True, a12=97.9, nvz=32, ensembles=64, dt_evo=1e-5)
+#v2, p2 = test(noise=False, gpu=True, a12=97.9, nvz=32, ensembles=64, dt_evo=1e-5)
+#XYPlot([p1, p2]).save("1d_population.pdf")
+
+# showcase 1: visibility(t) & N1(t) for different lattice sizes (no diffusion, no initial noise)
+#results = [test(noise=False, wigner=False, gpu=True, nvz=nvz, a12=97.9)
+#	for nvz in [16,32,64,128,256,512,1024,2048]]
+#XYPlot([r[0] for r in results], title="GPEs, visibility", gradient=True).save("1_visibility_979.pdf")
+#XYPlot([r[1] for r in results], title="GPEs, N1", gradient=True).save("1_population_979.pdf")
+#results = [test(noise=False, wigner=False, gpu=True, nvz=nvz, a12=80.8)
+#	for nvz in [16,32,64,128,256,512,1024,2048]]
+#XYPlot([r[0] for r in results], title="GPEs, visibility", gradient=True).save("1_visibility_808.pdf")
+#XYPlot([r[1] for r in results], title="GPEs, N1", gradient=True).save("1_population_808.pdf")
+
+# showcase 2: visibility(t) & N1(t) for different time steps (no diffusion, no initial noise)
+#results = [test(noise=False, wigner=False, gpu=True, nvz=1024, a12=97.9, dt_evo=dt)
+#	for dt in [4e-5, 2e-5, 1e-5, 4e-6, 2e-6, 1e-6, 4e-7]]
+#XYPlot([r[0] for r in results], title="GPEs, visibility", gradient=True).save("2_visibility_979.pdf")
+#XYPlot([r[1] for r in results], title="GPEs, N1", gradient=True).save("2_population_979.pdf")
+#results = [test(noise=False, wigner=False, gpu=True, nvz=1024, a12=80.8, dt_evo=dt)
+#	for dt in [4e-5, 2e-5, 1e-5, 4e-6, 2e-6, 1e-6, 4e-7]]
+#XYPlot([r[0] for r in results], title="GPEs, visibility", gradient=True).save("2_visibility_808.pdf")
+#XYPlot([r[1] for r in results], title="GPEs, N1", gradient=True).save("2_population_808.pdf")
+
+# showcase 3: visibility(t) & N1(t) for different lattice sizes (no diffusion, but with initial noise)
+#results = [test(noise=False, wigner=True, ensembles=512, gpu=True, nvz=nvz, a12=97.9)
+#	for nvz in [16,32,64,128,256]]
+#XYPlot([r[0] for r in results], title="Wigner + vacuum noise, visibility", gradient=True).save("3_visibility_979.pdf")
+#XYPlot([r[1] for r in results], title="Wigner + vacuum noise, N1", gradient=True).save("3_population_979.pdf")
+#results = [test(noise=False, wigner=True, ensembles=512, gpu=True, nvz=nvz, a12=80.8)
+#	for nvz in [16,32,64,128,256]]
+#XYPlot([r[0] for r in results], title="Wigner + vacuum noise, visibility", gradient=True).save("3_visibility_808.pdf")
+#XYPlot([r[1] for r in results], title="Wigner + vacuum noise, N1", gradient=True).save("3_population_808.pdf")
+
+# showcase 4: visibility(t) & N1(t) - divergence of trajectories (no diffusion, but with initial noise)
+results = [test(noise=False, wigner=True, gpu=True, nvz=32, ensembles=512, a12=97.9)[0]
+	for i in xrange(10)]
+XYPlot(results, legend=False, title=results[0].name).save("4_visibility_979.pdf")
+results = [test(noise=False, wigner=True, gpu=True, nvz=32, ensembles=512, a12=80.8)[0]
+	for i in xrange(10)]
+XYPlot(results, legend=False, title=results[0].name).save("4_visibility_808.pdf")
