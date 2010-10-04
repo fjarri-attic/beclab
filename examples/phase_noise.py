d83da8a9e15f481ac1e183bc0afc01c68e874db0
Bogdan Opanchuk <mantihor@gmail.com>
2010-10-05 02:03:48 +1100

    examples/phase_noise: added visibility collection

--- examples/phase_noise.py
+++ examples/phase_noise.py
@@ -7,7 +7,7 @@ from beclab.state import ParticleStatistics
 
 def testPhaseNoise(gpu):
 	constants = Constants(Model(N=40000, detuning=-41, nvx=16, nvy=16, nvz=128,
-		ensembles=4, e_cut=1e6), double=(False if gpu else True))
+		ensembles=16, e_cut=1e6), double=(False if gpu else True))
 	env = envs.cuda() if gpu else envs.cpu()
 	evolution = SplitStepEvolution(env, constants)
 	pulse = Pulse(env, constants)
@@ -24,7 +24,7 @@ def testPhaseNoise(gpu):
 	pulse.apply(cloud, 0.5 * math.pi, matrix=True)
 
 	t1 = time.time()
-	evolution.run(cloud, 0.05, callbacks=[n], callback_dt=0.01, noise=False)
+	evolution.run(cloud, 0.05, callbacks=[n, a], callback_dt=0.01, noise=False)
 	env.synchronize()
 	t2 = time.time()
 	print "Time spent: " + str(t2 - t1) + " s"
@@ -32,11 +32,11 @@ def testPhaseNoise(gpu):
 	#times, Na, Nb, N = b.getData()
 	#XYPlot([XYData("test", times, (Na-Nb)/N, ymin=-1, ymax=1, xname="Time, s")]).save('test.pdf')
 
-	#times, vis = a.getData()
-	#vis = XYData("noise", times, vis, ymin=0, ymax=1,
-	#	xname="Time, ms", yname="Visibility")
-	#vis = XYPlot([vis])
-	#vis.save('test.pdf')
+	times, vis = a.getData()
+	vis = XYData("noise", times, vis, ymin=0, ymax=1,
+		xname="Time, ms", yname="Visibility")
+	vis = XYPlot([vis])
+	vis.save('phase_noise_visibility_' + str(env) + '.pdf')
 
 	times, noise = n.getData()
 	XYPlot([XYData("test", times * 1000, noise, ymin=0, xname="Time, ms")]).save(
