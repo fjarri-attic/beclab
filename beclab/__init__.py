7f54ede801d1a6f5731456eaa87a97f7ccb0417e
Bogdan Opanchuk <mantihor@gmail.com>
2010-10-05 01:37:11 +1100

    __init__: added environment wrapper and common prelude for kernels

--- beclab/__init__.py
+++ beclab/__init__.py
@@ -1,8 +1,44 @@
 from . import helpers
 
+_PRELUDE = """
+#define DEFINE_INDEXES unsigned int index = GLOBAL_ID_FLAT, cell_index = index % ${c.cells}
+"""
+
+def getEnvWrapper(env_class):
+
+	class Temp(env_class):
+		def compileProgram(self, source, constants, **kwds):
+			from mako.template import Template
+			prelude = Template(_PRELUDE).render(c=constants, **kwds)
+
+			return self.compile(source, double=constants.double,
+				prelude=prelude, c=constants, **kwds)
+
+	Temp.__name__ = env_class.__name__
+	return Temp
+
+
+class _Envs:
+
+	def __init__(self):
+		try:
+			import helpers.cl
+			self.cl = getEnvWrapper(helpers.cl.CLEnvironment)
+		except:
+			pass
+
+		try:
+			import helpers.cuda
+			self.cuda = getEnvWrapper(helpers.cuda.CUDAEnvironment)
+		except:
+			pass
+
+		import helpers.cpu
+		self.cpu = helpers.cpu.CPUEnvironment
+
+envs = _Envs()
+
 from .model import Model
-from helpers.cpu import CPUEnvironment
-from helpers.cl import CLEnvironment
 from .constants import Constants, COMP_1_minus1, COMP_2_1
 from .ground_state import GPEGroundState
 from .evolution import Pulse, SplitStepEvolution, SplitStepEvolution2, RungeKuttaEvolution, RK4Evolution
