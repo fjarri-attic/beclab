a6a15543c2e1fe01186f39838a321303848d9488
Bogdan Opanchuk <mantihor@gmail.com>
2010-10-05 01:31:34 +1100

    helpers/__init__: added default imports

---
+++ beclab/helpers/__init__.py
@@ -0,0 +1,5 @@
+from .fft import createFFTPlan
+from .transpose import createTranspose
+from .reduce import createReduce
+from .misc import PairedCalculation, log2
+from .typenames import double_precision, single_precision
