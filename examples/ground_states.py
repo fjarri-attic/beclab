fe107cba9df57b92994df8c2ae636732739bb579
Bogdan Opanchuk <mantihor@gmail.com>
2011-07-07 16:36:13 +1000

    examples/ground_states: added various checks for atom number

--- examples/ground_states.py
+++ examples/ground_states.py
@@ -30,16 +30,18 @@ def testThomasFermi(gpu, grid_type, dim):
 
 	psi = tf.create(N)
 
-	# check that total population is correct
-	N_xspace = stats.getN(psi)
-	print "Total population:", N_xspace
+	# population in x-space
+	N_xspace1 = stats.getN(psi)
 
-	# test normalization in mode space
+	# population in mode space
 	psi.toMSpace()
-
 	N_mspace = stats.getN(psi)
-	print "Total population in mode space:", N_mspace
 	psi.toXSpace()
+
+	# population in x-space after double transformation (should not change)
+	N_xspace2 = stats.getN(psi)
+
+	print "N(x-space) = {Nx}, N(m-space) = {Nm}".format(Nx=N_xspace2, Nm=N_mspace)
 
 	z = grid.z
 	profile = prj.getZ(psi) / 1e6
@@ -47,6 +49,17 @@ def testThomasFermi(gpu, grid_type, dim):
 		xname="Z ($\\mu$m)", yname="Axial density ($\\mu$m$^{-1}$)", ymin=0)
 
 	env.release()
+
+	# Checks
+
+	# check that double transform did not change N
+	assert abs(N_xspace1 - N_xspace2) / N_xspace2 < 1e-6
+	# check that we actually got N we asked for
+	assert abs(N_xspace2 - N) / N < 1e-6
+	# There is certain difference for harmonic grid,
+	# because FHT does not conserve population (TODO: prove it mathematically)
+	assert abs(N_mspace - N_xspace2) / N_xspace2 < 2e-2 if grid_type == 'harmonic' else 1e-6
+
 	return plot
 
 if __name__ == '__main__':
