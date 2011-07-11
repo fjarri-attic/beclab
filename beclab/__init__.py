a230744f07e2b2d3dc0426b36142988099ae0de9
Bogdan Opanchuk <mantihor@gmail.com>
2011-07-12 01:58:42 +1000

    beclab/ground_state: added RK5HarmonicGroundState

--- beclab/__init__.py
+++ beclab/__init__.py
@@ -45,7 +45,7 @@ envs = _Envs()
 
 from .model import Model
 from .constants import Constants, UniformGrid, HarmonicGrid
-from .ground_state import RK5IPGroundState, SplitStepGroundState, TFGroundState
+from .ground_state import RK5HarmonicGroundState, RK5IPGroundState, SplitStepGroundState, TFGroundState
 #from .evolution import SplitStepEvolution
 #from .pulse import Pulse
 #from .collectors import AxialProjectionCollector, ParticleNumberCollector, \
