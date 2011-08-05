603888f6420776348502d5c68012332f15a2a4e2
Bogdan Opanchuk <mantihor@gmail.com>
2011-08-05 17:19:12 +1000

    examples/convergence: cosmetical changes, added RK5IPEvolution

--- examples/convergence.py
+++ examples/convergence.py
@@ -48,6 +48,9 @@ def runTest(env, grid_type, dim, prop_type, repr_type):
 		'3d': [1e-4, 4e-5, 2e-5, 1e-5, 4e-6]
 	}[dim]
 
+	# relative tolerances for RK5
+	rk_epss = [1e-3, 1e-4, 1e-5, 1e-6, 1e-7]
+
 	ensembles = 128
 
 	wigner = (repr_type == 'wigner')
@@ -63,7 +66,14 @@ def runTest(env, grid_type, dim, prop_type, repr_type):
 	final_N = []
 	vis_plots = []
 
-	for ss_dt in ss_dts:
+	if prop_type == 'split-step':
+		params = ss_dts
+		pname = 'dt'
+	elif prop_type == 'rk5':
+		params = rk_epss
+		pname = 'eps'
+
+	for param in params:
 		psi = gs.create((total_N, 0))
 
 		if wigner:
@@ -71,11 +81,16 @@ def runTest(env, grid_type, dim, prop_type, repr_type):
 
 		p = ParticleNumberCollector(env, constants, grid, pulse=pulse)
 		v = VisibilityCollector(env, constants, grid)
-		evolution = SplitStepEvolution(env, constants, grid, dt=ss_dt)
+
+		if prop_type == 'split-step':
+			evolution = SplitStepEvolution(env, constants, grid, dt=param)
+		elif prop_type == 'rk5':
+			evolution = RK5IPEvolution(env, constants, grid, Nscale=total_N,
+				atol_coeff=1e-3, eps=param)
 
 		pulse.apply(psi, theta=0.5 * numpy.pi)
 
-		evolution.run(psi, 0.2, callbacks=[p, v], callback_dt=0.005)
+		evolution.run(psi, 1.0, callbacks=[p, v], callback_dt=0.005)
 		env.synchronize()
 
 		times, vis = v.getData()
@@ -84,11 +99,11 @@ def runTest(env, grid_type, dim, prop_type, repr_type):
 		final_vis.append(vis[-1])
 		final_N.append(Ntotals[-1])
 
-		print "  dt = {ss_dt}: N = {N:.4f}, V = {V:.4f}".format(
-			ss_dt=ss_dt, N=final_N[-1], V=final_vis[-1])
+		print "  {pname} = {param}: N = {N:.4f}, V = {V:.4f}".format(
+			pname=pname, param=param, N=final_N[-1], V=final_vis[-1])
 
 		vis_plots.append(XYData(
-			"dt = " + str(ss_dt), times * 1e3, vis,
+			pname + " = " + str(param), times * 1e3, vis,
 			xname="T (ms)", yname="$\\mathcal{V}$", ymin=0, ymax=1
 		))
 
@@ -104,7 +119,7 @@ if __name__ == '__main__':
 
 	tests = (
 		('uniform',), # grid type
-		('split-step',), # propagation type
+		('split-step', 'rk5',), # propagation type
 		('classical', 'wigner',), # representation type
 	)
 
