b422d914a8bc6c6e0adbf0aa1d70fdc59ebda856
Bogdan Opanchuk <mantihor@gmail.com>
2011-08-17 22:38:10 +1000

    examples/evolution_graph: updated

--- examples/evolution_graph.py
+++ examples/evolution_graph.py
@@ -1,55 +1,41 @@
-import numpy
-import time
-import math
+"""
+Ramsey sequence and cloud snapshots during the evolution
+"""
 
+import numpy
 from beclab import *
 
-def testEvolutionGraph(gpu):
+N = 150000
 
-	# preparation
-	env = envs.cuda() if gpu else envs.cpu()
-	constants = Constants(Model(N=150000), double=False if gpu else True)
-	gs = GPEGroundState(env, constants)
-	evolution = SplitStepEvolution(env, constants)
-	pulse = Pulse(env, constants)
-	a = SurfaceProjectionCollector(env, constants, pulse=pulse)
+env = envs.cuda()
+constants = Constants(double=env.supportsDouble())
+grid = UniformGrid.forN(constants, N, (128, 16, 16))
 
-	# experiment
-	cloud = gs.createCloud()
-	pulse.apply(cloud, theta=0.5*math.pi)
-	t1 = time.time()
-	evolution.run(cloud, time=0.119, callbacks=[a], callback_dt=0.005)
-	env.synchronize()
-	t2 = time.time()
-	print "Time spent: " + str(t2 - t1) + " s"
+gs = SplitStepGroundState(env, constants, grid, dt=1e-5)
+#evolution = RK5IPEvolution(env, constants, grid)
+evolution = SplitStepEvolution(env, constants, grid, dt=1e-5)
+pulse = Pulse(env, constants, grid, f_rabi=350, f_detuning=41)
+a = SurfaceProjectionCollector(env, constants, grid, pulse=pulse)
 
-	# render
-	times, a_xy, a_yz, b_xy, b_yz = a.getData()
+# run simulation
+psi = gs.create((N, 0))
+pulse.apply(psi, theta=numpy.pi / 2)
+evolution.run(psi, 0.119, callbacks=[a], callback_dt=0.005)
+env.release()
 
-	times = [str(int(x * 1000)) for x in times]
+# build plot
+times, xy, yz = a.getData()
+times = [str(int(x * 1000)) for x in times]
 
-	def constructPlot(dataset, dl):
-		hms = []
+# right after pulse second component has all the population,
+# so we are estimating maximum density based on its center
+max_density = yz[1, 0, grid.shape[0] / 2, grid.shape[1] / 2]
+for comp in (0, 1):
+	hms = []
+	for t, hm in zip(times[:-1], yz[comp,:-1]):
+		hms.append(HeightmapData(t, hm.transpose(),
+			xmin=grid.z[0], xmax=grid.z[-1],
+			ymin=grid.y[0], ymax=grid.y[-1],
+			zmin=0, zmax=max_density))
 
-		# good approximation for the scale: density in the middle of TF ground state
-		max_density = constants.muTF() / constants.g[(COMP_1_minus1, COMP_1_minus1)] * dl
-
-		for t, hm in zip(times, dataset):
-			hms.append(HeightmapData(t, hm.transpose(),
-				xmin=-constants.zmax, xmax=constants.zmax,
-				ymin=-constants.ymax, ymax=constants.ymax,
-				zmin=0, zmax=max_density))
-
-		return EvolutionPlot(hms, shape=(6, 4))
-
-	res = constructPlot(a_yz, constants.xmax), constructPlot(b_yz, constants.xmax)
-	env.release()
-	return res
-
-p1, p2 = testEvolutionGraph(gpu=False)
-p1.save("evolution_cpu_a.pdf")
-p2.save("evolution_cpu_b.pdf")
-
-p1, p2 = testEvolutionGraph(gpu=True)
-p1.save("evolution_gpu_a.pdf")
-p2.save("evolution_gpu_b.pdf")
+	EvolutionPlot(hms, shape=(6, 4)).save('evolution_snapshots_comp' + str(comp + 1) + '.pdf')
