bd3bcbac71a4311ad770484177eb1d48454d58d6
Bogdan Opanchuk <mantihor@gmail.com>
2011-09-19 15:15:44 +1000

    examples: updated SO examples

--- examples/so_cutoff.py
+++ examples/so_cutoff.py
@@ -1,7 +1,8 @@
 import numpy
 from beclab import *
 from beclab.constants import SOConstants2D
-from beclab.ground_state_so import SOGroundStateEvo
+from beclab.constants import _HBAR as hbar
+from beclab.ground_state_so import SOGroundState
 from beclab.meters import ParticleStatistics
 import time
 
@@ -52,30 +53,33 @@ def buildPlot(na, nb, filename, **plot_params):
 	fig.savefig(filename)
 
 def runTest(g_ratio, g_strength, lambda_SO, grid_size=2048, area_size=6,
-		precision=1e-7, dt_dimensionless=1.0/50, random_init=True):
+		precision=1e-7, dt_dimensionless=1.0/50, random_init=True, k_cutoff=1024):
 
 	so_constants = SOConstants2D(g_ratio=g_ratio, g_strength=g_strength, lambda_SO=lambda_SO)
+	e_cut = hbar * (k_cutoff / so_constants.a_perp) ** 2 / (2.0 * so_constants.m)
 
 	env = envs.cuda()
 	constants = Constants(double=env.supportsDouble(),
 		g_intra=so_constants.g_intra, g_inter=so_constants.g_inter,
 		lambda_R=so_constants.lambda_R, m=so_constants.m,
-		fx=so_constants.f_perp, fy=so_constants.f_perp)
+		fx=so_constants.f_perp, fy=so_constants.f_perp,
+		e_cut=e_cut)
 
 	box_size = (so_constants.a_perp * area_size, so_constants.a_perp * area_size)
-	grid = UniformGrid(env, constants, (grid_size, grid_size), box_size)
+	grid = UniformGrid(constants, (grid_size, grid_size), box_size)
 
 	t_scale = 1.0 / (so_constants.f_perp * numpy.pi * 2)
 	E_scale = constants.hbar * so_constants.f_perp * numpy.pi * 2
 	E_modifier = so_constants.lambda_SO ** 2 / 2 * E_scale
 	print "time scale = ", t_scale
-	gs = SOGroundStateEvo(env, constants, grid, dt=t_scale * dt_dimensionless, components=2)
-	stats = gs._stats
+	gs = SOGroundState(env, constants, grid, dt=t_scale * dt_dimensionless,
+		components=2, precision=precision, E_modifier=E_modifier, random_init=random_init)
+	stats = gs._statistics
 
 	N = so_constants.N
 	print "target N = ", N
 	t1 = time.time()
-	psi = gs.create(N, precision=precision, E_modifier=E_modifier, random_init=random_init)
+	psi = gs.create((N / 2, N / 2))
 	print "creation time = ", time.time() - t1
 
 	Ns = stats.getN(psi)
@@ -96,27 +100,24 @@ def runTest(g_ratio, g_strength, lambda_SO, grid_size=2048, area_size=6,
 		xname="$x / a_{\\perp}$", yname="$y / a_{\\perp}$", zname="density",
 		zmin=0, zmax=n.max(),
 		title=(
-			"$\\lambda_{{SO}} = {lambda_SO}$, $g_{{\\uparrow\\downarrow}} / g = {g_ratio}$, $g N = {g_strength}$\n" +
+			"$\\lambda_{{SO}} = {lambda_SO}$, $g_{{\\uparrow\\downarrow}} / g = {g_ratio}$, $g N = {g_strength}$, $k_{cut} = {k_cutoff}$\n" +
 			"grid: ${gsize}\\times{gsize}$, $dt = {dt} / \\omega_{{\\perp}}$, $precision = {precision}$, " + ("random" if random_init else "uniform") + " initial conditions\n" +
 			"$E / N + \\lambda_{{SO}}^2 / 2 = {E:.6f}$, $n_P = {n_P:.4f}$, $n_{{PT}} = {n_PT:.4f}$"
 			).format(
-				lambda_SO=so_constants.lambda_SO, g_ratio=so_constants.g_ratio,
+				lambda_SO=so_constants.lambda_SO, g_ratio=so_constants.g_ratio, k_cutoff=k_cutoff,
 				g_strength=so_constants.g_strength,
 				gsize=grid_size, dt=dt_dimensionless, precision=precision,
 				E=E_shifted, n_P=p, n_PT=pt
 			)
 	)
 
-	suffix = "{g_ratio}_{g_strength}_{lambda_SO}_{init}".format(
+	suffix = "{g_ratio}_{g_strength}_{lambda_SO}_k{k_cutoff}_{init}".format(
 		g_ratio=so_constants.g_ratio, g_strength=so_constants.g_strength,
-		lambda_SO=so_constants.lambda_SO,
+		lambda_SO=so_constants.lambda_SO, k_cutoff=k_cutoff,
 		init=("random" if random_init else "uniform"))
 	buildPlot(n[0, 0], n[1, 0], 'so_' + suffix + '.pdf', **plot_params)
 
 if __name__ == '__main__':
-#	runTest(1.5, 0.2, 20, dt_dimensionless=1.0/800)
-#	runTest(1.5, 1.0, 4)
-	runTest(0.43, 1.5, 4, grid_size=128, precision=1e-5)
-#	runTest(0.43, 2.0, 4, grid_size=512, random_init=True, precision=1e-8)
-#	runTest(1.5, 0.2, 20, dt_dimensionless=1.0/800, area_size=4, random_init=False)
-#	runTest(1.5, 0.2, 20, dt_dimensionless=1.0/800, area_size=4, random_init=True)
+	for k_cutoff in (256, 512):
+		runTest(1.5, 0.2, 20, grid_size=1024, dt_dimensionless=1.0/50,
+			area_size=4, random_init=False, k_cutoff=k_cutoff)
